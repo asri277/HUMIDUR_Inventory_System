0ee96c6647c91d1b237a68f96f4e8f98eb7afdb4
asri277 <81425138+asri277@users.noreply.github.com>
2021-09-09 14:21:09 +0800

    added index.php + modified login.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>HUMIDUR INVENTORY SYSTEM</title>
+  </head>
+  <body>
+
+    <h1 id="ht1" class="header_title">WELCOME</h1>
+    <h1 class="header_title">HUMIDUR INVENTORY SYSTEM</h1>
+
+    <div class="login_button_index">
+      <a class="link_button_index" href="login/login.php">LOGIN</a>
+    </div>
+
+  </body>
+</html>
+
+<style type="text/css">
+  *{
+    margin: 0%;
+    padding: 0%;
+    background-color: #E0E0E0;
+  }
+
+  .header_title{
+    text-align: center;
+    color: #202020;
+    font-size: 300%;
+  }
+
+  #ht1{
+    /* border: 1px solid #000000; */
+    margin-top: 17%;
+  }
+
+  .login_button_index{
+    text-align: center;
+    box-shadow: 0px 1px 2px #404040;
+  }
+
+  .login_button_index .link_button_index{
+    /* border: 1px solid #000000; */
+    color: #000000;
+    display: block;
+    text-decoration: none;
+    padding: 2%;
+    margin-left: 40.5%;
+    margin-top: 1%;
+    width: 15%;
+    transition: 0.3s;
+  }
+
+  .login_button_index .link_button_index:hover{
+    background-color: #FFFFFF;
+  }
+</style>
--- login/login.php
+++ login/login.php
@@ -15,7 +15,7 @@
 		{
 			//read from database
 			$query = "select * from users where username = '$username' limit 1";
-			
+
 			$result = mysqli_query($con, $query);
 
 			if($result)
@@ -23,7 +23,7 @@
 				if($result && mysqli_num_rows($result) > 0)
 				{
 					$user_data = mysqli_fetch_assoc($result);
-					
+
 					if($user_data['password'] === $password)
 					{
 						$_SESSION['user_id'] = $user_data['user_id'];
@@ -33,10 +33,22 @@
 				}
 			}
 
-			echo "Incorrect username or password!";
-			
+			?>
+		    <script type="text/javascript">
+			    alert(' Incorrect username or password! ');
+			    window.location='../login/login.php';
+		    </script>
+	    <?php
+			// echo "Incorrect username or password!";
+
 		}else{
-			echo "Please enter valid information!";
+			?>
+		    <script type="text/javascript">
+			    alert(' Please enter valid information! ');
+			    window.location='../login/login.php';
+		    </script>
+	    <?php
+			// echo "Please enter valid information!";
 		}
 	}
 
@@ -45,56 +57,75 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>Login</title>
-</head>
-<body>
+	<head>
+		<meta charset="utf-8">
+		<title>Login</title>
+	</head>
+	<body>
 
-	<style type="text/css">
+		<div id="box">
 
-		#text{
+			<form method="post">
+				<div style="font-size: 20px;margin: 10px 0px 10px 0px;color: #000000;">Login</div>
+				<input placeholder="Username" id="text" type="text" name="username"><br><br>
+				<input placeholder="Password" id="text" type="password" name="password"><br><br>
 
-			height: 25px;
-			border-radius: 5px;
-			padding: 0px;
-			border: solid thin #aaa;
-			width: 100%;
-		}
+				<input id="button"type="submit" value="Login"><br><br>
 
-		#button{
+				<a class="link_button_login" href="signup.php">Register new account!</a><br><br>
+				<a class="link_button_login" href="../index.php">Back to Home</a>
+			</form>
+		</div>
 
+	</body>
+</html>
+
+<style type="text/css">
+	body{
+		background-color: #E0E0E0;
+	}
+
+	#text{
+		height: 25px;
+		border-radius: 5px;
+		padding: 0% 0% 0% 2%;
+		border: solid thin #aaa;
+		width: 100%;
+	}
+
+	#button{
 		padding: 10px;
 		width: 100px;
-		color: white;
-		background-color: lightblue;
+		color: #000000;
+		background-color: #FFFFFF;
 		border: none;
+		box-shadow: 0px 1px 2px #404040;
+		transition: 0.3s;
 	}
 
-		#box{
+	#button:hover{
+		background-color: lightblue;
+	}
 
-		background-color: grey;
-		margin: auto;
+	#box{
+		background-color: #FFFFFF;
+		margin-top: 11%;
+		margin-left: auto;
+		margin-right: auto;
 		width: 300px;
 		padding: 20px;
+		box-shadow: 0px 1px 2px #404040;
 	}
 
-	</style>
+	.link_button_login{
+		text-decoration: none;
+		color: #A0A0A0;
+		font-size: 80%;
+		transition: 0.3s;
+	}
 
-	<div id="box">
-		
-		<form method="post">
-			<div style="font-size: 20px;margin: 10px;color: white;">Login</div>
-			<input placeholder="Username" id="text" type="text" name="username"><br><br>
-			<input placeholder="Password" id="text" type="password" name="password"><br><br>
+	.link_button_login:hover{
+		color: blue;
+	}
 
-			<input id="button"type="submit" value="Login"><br><br>
-
-			<a href="signup.php">Register new account!</a><br><br>
-			<a href="home.php">Back to Home</a>
-		</form>
-	</div>
-	
-
-</body>
-</html>
+</style>
